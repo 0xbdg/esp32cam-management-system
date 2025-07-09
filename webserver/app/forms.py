6f0e06e0f439a59dde69c9877c207e90a9555f98
?0 <benjamindguntara@protonmail.com>
2025-07-10 03:02:50 +0700

    oidjasojdoaij

--- webserver/app/forms.py
+++ webserver/app/forms.py
@@ -4,8 +4,8 @@ from django.forms.widgets import *
 from .models import *
 
 class CCTVForm(forms.ModelForm):
-    name = forms.CharField(required=True, widget=TextInput(attrs={"class":""}))
-    cctv_url = forms.CharField(required=True, widget=URLInput(attrs={"class":""}))
+    name = forms.CharField(required=True, widget=TextInput(attrs={"class":"w-full bg-gray-800 border border-gray-700 px-3 py-2 text-sm focus:outline-none focus:border-blue-500", "placeholder":"e.g. Kitchen"}))
+    cctv_url = forms.CharField(required=True, widget=URLInput(attrs={"class":"w-full bg-gray-800 border border-gray-700 px-3 py-2 text-sm focus:outline-none focus:border-blue-500", "placeholder":"http://192.168.x.x:81"}))
 
     class Meta:
         model = CCTV
--- webserver/app/views.py
+++ webserver/app/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 from .forms import *
 # Create your views here.
 
 def index(request):
+    form = None
     if request.method == "POST":
-        pass
+        form = CCTVForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+
+    else:
+        form = CCTVForm()
     cctv = CCTV.objects.all()
-    return render(request, "index.html", {"cctv":cctv})
+    return render(request, "index.html", {"cctv":cctv, "form":form})
